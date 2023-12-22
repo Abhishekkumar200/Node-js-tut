0930158a81d53dccba3819fee2650eb6785f77e6
Abhishek Kumar <84954320+Abhishekkumar200@users.noreply.github.com>
2023-12-22 15:41:04 +0530

    Create put-method.js
    This file contains code for updating data in mysql database using node.js.

---
+++ Node.js-and-MySql/put-method.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const con = require("./config");
+const app = express();
+
+app.use(express.json());
+
+app.get("/data", (req, res)=>{
+  con.query("select * from students;", (err, result)=>{
+    if(err)
+    {
+      res.send(err);
+    }
+    res.send(result);
+  });
+});
+
+app.post("/", (req, res)=>{
+  const data = req.body;
+  con.query("INSERT INTO students SET ?", data, (err, result, fields)=>{
+    if(err)
+    {
+      res.send(err);
+    }
+    else{
+      res.send(result);
+    }
+
+  });
+});
+
+app.put("/update", (req, res)=>{
+  // res.send("Working");
+  const data = ["Laxmikant", 21, "Ajmer","Male", "Laxmikant"];
+  con.query("UPDATE students SET studentName = ?, age = ?, city = ?, gender = ? where studentName = ?", data, (err, result, field)=>{
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+app.listen(5000);
+
+// con.query("", (err, result)=>{
+//   console.warn("result", result);
+// })
+
